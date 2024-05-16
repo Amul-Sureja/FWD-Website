55e726a0dc2fd5c5e757c0c4b7d779661dea62a0
Amul <amulsurejapi@gmail.com>
2024-05-16 22:31:18 +0530

    News

---
+++ Script.js
@@ -0,0 +1,21 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+  // alert("Plz Subscribe ");
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
+// const container = document.getElementById('container');
+// const registerBtn = document.getElementById('register');
+// const loginBtn = document.getElementById('login');
+
+// registerBtn.addEventListener('click', () => {
+//     container.classList.add("active");
+// });
+
+// loginBtn.addEventListener('click', () => {
+//     container.classList.remove("active");
+// });
